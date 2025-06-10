1c49f4e2b83185a7f8da9c3c84130eb8a77ff78a
Nakul Kamath <69424522+NakulKamath@users.noreply.github.com>
2025-06-10 21:35:39 +0530

    RC-Final v0.1.0 release

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 const SignInPage = () => {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 overflow-hidden">
       <div className="flex w-full max-w-sm flex-col gap-6 pb-[10vh]">
           <Link href="/" className="flex items-center gap-2 self-center font-medium">
             <LeeterboardLogo />
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 const SignUpPage = () => {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 pverflow-hidden">
       <div className="flex w-full max-w-sm flex-col gap-6 pb-[10vh]">
           <Link href="/" className="flex items-center gap-2 self-center font-medium">
             <LeeterboardLogo />
